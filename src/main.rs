97a09caa17d469a6afe1cd466d3e5f07a3d5bb08
jackzhhuang <jackzhhuang@gmail.com>
2023-05-22 16:09:28 +0800

    request and respon debug successfully

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::{sync::Arc, net::Ipv4Addr, borrow::Cow, time::Duration};
 use futures::{channel::mpsc::{self, Receiver}, StreamExt};
 use network_p2p::{config, NetworkService, NetworkWorker};
-use network_p2p_types::{IfDisconnected, IncomingRequest, OutgoingResponse, ReputationChange};
+use network_p2p_types::{IfDisconnected, IncomingRequest, OutgoingResponse, ReputationChange, PeerId, parse_str_addr};
 use serde::{Deserialize, Serialize};
 use bcs_ext::BCSCodec;
 use network_p2p::request_responses::ProtocolConfig;
 use tokio::task::JoinHandle;
 
-const PROTOCOL_NAME: &str = "/starcoin/notify/1";
+const PROTOCOL_NAME_CHAIN: &str = "/starcoin/chain/1";
+const PROTOCOL_NAME_NOTIFY: &str = "/starcoin/notify/1";
+const PROTOCOL_NAME_REQUEST_RESPONSE: &str = "/starcoin/reques_response/1";
 
 #[derive(Debug, Serialize, Deserialize, Eq, PartialEq, Clone)]
 pub struct MyReqeust {
@@ -25,7 +27,7 @@ pub struct MyResponse {
 fn generate_protocol_config() -> (ProtocolConfig, Receiver<IncomingRequest>) {
     let (sender, receiver) = mpsc::channel(128);
     (ProtocolConfig {
-        name: Cow::from(PROTOCOL_NAME),
+        name: Cow::from(PROTOCOL_NAME_REQUEST_RESPONSE),
         max_request_size: 1024 * 1024 * 1024 * 10,
         max_response_size: 1024 * 1024 * 1024 * 10,
         request_timeout: Duration::from_secs(10),
@@ -39,7 +41,7 @@ async fn build_worker(
     println!("config: {:?}", config);
     let worker = NetworkWorker::new(config::Params {
         network_config: config,
-        protocol_id: config::ProtocolId::from(PROTOCOL_NAME),
+        protocol_id: config::ProtocolId::from(PROTOCOL_NAME_CHAIN),
         metrics_registry: None,
     })
     .unwrap();
@@ -65,7 +67,7 @@ pub async fn build_network() -> (Arc<NetworkService>, JoinHandle<()>, Receiver<I
     // protocol_config.inbound_queue = Some(sender);
     
     let (service, handle)= build_worker(config::NetworkConfiguration {
-        notifications_protocols: vec![From::from(PROTOCOL_NAME)],
+        notifications_protocols: vec![From::from(PROTOCOL_NAME_NOTIFY)],
         listen_addresses: vec![listen_addr.clone()],
         transport: config::TransportConfig::Normal { enable_mdns: true, allow_private_ip: true },
         request_response_protocols: vec![protocol_config],
@@ -78,104 +80,61 @@ pub async fn build_network() -> (Arc<NetworkService>, JoinHandle<()>, Receiver<I
 fn main() {
     let mut rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(async {
-        let (service1, handle1, mut receiver1) = build_network().await;
-        let (service2, handle2, mut receiver2) = build_network().await;
+        let (service, worker_handle, mut receiver) = build_network().await;
 
-        println!("peer id 1 = {:?}, peer id 2 = {:?}", service1.peer_id(), service2.peer_id());
+        println!("peer id = {:?}", service.peer_id());
 
-        let state1 = service1.network_state().await.unwrap();
-        println!("state1 = {:?}", state1.listened_addresses);
+        std::thread::sleep(Duration::from_secs(3));
 
-        let state2 = service2.network_state().await.unwrap();
-        println!("state2 = {:?}", state2.listened_addresses);
+        let state = service.network_state().await.unwrap();
+        println!("state = {:?}", state);
 
-        let peer_id = state2.peer_id.clone();
-        state2.listened_addresses.iter().for_each(|multiaddr| {
-            let addr = format!("{}/p2p/{}", multiaddr.to_string(), peer_id.to_string());
-            println!("addr = {}", addr);
-            service1.add_reserved_peer(addr).unwrap();
+        let receive_handle = tokio::task::spawn(async move {
+            loop {
+                println!("waiting from a client");
+                match receiver.next().await {
+                    Some(request) => {
+                        println!("get from a client");
+                        let peer_request = MyReqeust::decode(&request.payload).unwrap();
+                        println!("receive the request from a peer: {peer_request:?}");
+                        let message = MyResponse {
+                            number: 1002,
+                            name: String::from("pong"),
+                        };
+                        let res = OutgoingResponse {
+                            result: Ok(message.encode().unwrap()),
+                            reputation_changes: vec![ReputationChange::new(100, "test reputation change")],
+                        };
+                        request.pending_response.send(res).unwrap();
+                    }
+                    None => ()
+                }
+            }
         });
 
-        loop {
-            if service1.is_connected(service2.peer_id().clone()).await {
-                println!("service 1 and service 2 is connected!");
-                break;
-            }         
-        }
-
-            let req = MyReqeust {
-                number: 1001,
-                name: String::from("jack"), 
-            };
-
-            let handle3 = tokio::task::spawn(async move {
-                loop {
-                    println!("waiting from a client");
-                    match receiver2.next().await {
-                        Some(request) => {
-                            println!("get from a client");
-                            let message = MyResponse {
-                                number: 1002,
-                                name: String::from("rose"),
-                            };
-                            let res = OutgoingResponse {
-                                result: Ok(message.encode().unwrap()),
-                                reputation_changes: vec![ReputationChange::new(100, "test reputation change")],
-                            };
-                            request.pending_response.send(res).unwrap();
-                        }
-                        None => ()
-                    }
-                }
-            });
-            let handle4 = tokio::task::spawn(async move {
-                loop {
-                    println!("waiting from a client");
-                    match receiver1.next().await {
-                        Some(request) => {
-                            println!("get from a client");
-                            let message = MyResponse {
-                                number: 1002,
-                                name: String::from("rose"),
-                            };
-                            let res = OutgoingResponse {
-                                result: Ok(message.encode().unwrap()),
-                                reputation_changes: vec![ReputationChange::new(100, "test reputation change")],
-                            };
-                            request.pending_response.send(res).unwrap();
-                        }
-                        None => ()
-                    }
-                }
-            });
+        if let Some(addr) = std::env::args().nth(1) {
+            let (peer_id, _) = parse_str_addr(&addr).unwrap();
+            service.add_reserved_peer(addr).unwrap();
 
             loop {
-                
-                let result = service1.request(service2.peer_id().clone(), Cow::from(PROTOCOL_NAME), req.encode().unwrap(), IfDisconnected::TryConnect).await;
-                println!("result = {:?}", result);
-                if service1.is_connected(service2.peer_id().clone()).await {
-                    println!("service 1 and service 2 is connected!");
+                if service.is_connected(peer_id).await {
+                    println!("service is connected to {peer_id:?}!");
+                    break;
                 }         
             }
+             let req = MyReqeust {
+                number: 1001,
+                name: String::from("ping"), 
+            };
 
-            handle1.await;
-            handle2.await;
-            handle3.await;
-            handle4.await;
-        // let stream = receiver1.map(move |request| {
-        //     let message = MyResponse {
-        //         number: 1002,
-        //         name: String::from("rose"),
-        //     };
-        //     let response = OutgoingResponse {
-        //         result: Ok(message.encode().unwrap()),
-        //         reputation_changes: vec![ReputationChange::new(100, "test reputation change")],
-        //     };
-        //     let result = request.pending_response.send(response);
-        //     result.unwrap();
-        // });
-        // let _ = stream.fuse();
-
-        // handle.await.unwrap();
+            loop {
+                let result = service.request(peer_id, Cow::from(PROTOCOL_NAME_REQUEST_RESPONSE), req.encode().unwrap(), IfDisconnected::TryConnect).await.unwrap();
+                let response = MyResponse::decode(&result);
+                println!("result = {:?}", response);
+            }
+               
+        }
+        receive_handle.await;
+        worker_handle.await;
     })
 }
